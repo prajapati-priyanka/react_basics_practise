f76bff8cfd69717f051eb1d1062bff39751dfa03
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2025-08-09 02:05:35 +0530

    feat: Integrated API on the cards

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,35 @@
+import { useEffect, useState } from "react";
 import RestaurantCard from "../RestaurantCard";
 import "./Main.css";
+import axios from "axios";
 
 const Main = () => {
+  const [cardData, setCardData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://swiggy-api-4c740.web.app/swiggy-api.json"
+    );
+    const { data, status } = response;
+    if (status === 200) {
+      const responseData =
+        data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
+      setCardData(responseData);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+//   console.log(cardData[0])
   return (
     <main className="main-container">
       <h3 className="main-heading">Restaurants Available to Serve Now</h3>
       <section className="res-card-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        {cardData?.map((card) => (
+          <RestaurantCard key={card?.info?.id} resData={card.info} />
+        ))}
       </section>
     </main>
   );
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,18 +1,21 @@
 import "./Main/Main.css";
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    const {resData} = props;
+    const {name,avgRating,sla:{slaString}, cuisines,locality} = resData;
+    console.log("resData", resData);
   return (
     <div className="res-card">
       <figure className="res-image">
         <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/8/24/8dcf7245-861c-4c67-8d4c-a73927422e62_946888.jpg" />
       </figure>
       <div className="res-body">
-        <p className="res-name">Apni Chopati</p>
-        <span className="res-rating">3.6</span> *{" "}
-        <span className="res-delivery-time">40-45 mins</span>
+        <p className="res-name">{name}</p>
+        <span className="res-rating">{avgRating}</span> *{" "}
+        <span className="res-delivery-time">{slaString}</span>
         <p className="res-cuisines">
-          Pizzas, Italian Pastas, Desserts, North India
+          {cuisines.join(",")}
         </p>
-        <p className="res-address">Hinjewadi Chowk, Pune</p>
+        <p className="res-address">{locality}</p>
       </div>
     </div>
   );
