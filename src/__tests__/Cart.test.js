866aca901ae52f983cda8d45a1dc770758e9b0aa
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2025-09-18 01:22:36 +0530

    Test: group all test cases of cart page

--- src/__tests__/Cart.test.js
+++ src/__tests__/Cart.test.js
@@ -2,31 +2,33 @@ import { render, screen } from "@testing-library/react";
 import Cart from "../components/Cart/Cart";
 import "@testing-library/jest-dom";
 
-test("Should render Cart Page", () => {
-  render(<Cart />);
+describe("Cart page render", () => {
+  test("Should render Cart Page", () => {
+    render(<Cart />);
 
-  //Quering
-  const heading = screen.getByRole("heading");
+    //Quering
+    const heading = screen.getByRole("heading");
 
-  // Assertion
-  expect(heading).toBeInTheDocument();
-});
+    // Assertion
+    expect(heading).toBeInTheDocument();
+  });
 
-test("Should render submit button", () => {
-  render(<Cart />);
+  test("Should render submit button", () => {
+    render(<Cart />);
 
-  //Quering
-  const button = screen.getByRole("button");
+    //Quering
+    const button = screen.getByRole("button");
 
-  // Assertion
-  expect(button).toBeInTheDocument();
-});
-test("Should get submit button text", () => {
-  render(<Cart />);
+    // Assertion
+    expect(button).toBeInTheDocument();
+  });
+  test("Should get submit button text", () => {
+    render(<Cart />);
 
-  //Quering
-  const button = screen.getByText("Submit");
+    //Quering
+    const button = screen.getByText("Submit");
 
-  // Assertion
-  expect(button).toBeInTheDocument();
+    // Assertion
+    expect(button).toBeInTheDocument();
+  });
 });
