cd0c546710ae1da40ba30b5b1c5a3f190a3616b0
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2025-09-24 00:01:22 +0530

    Test: Header component renders and Cart in the Header

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -6,6 +6,12 @@ import { BrowserRouter } from "react-router";
 it("Should render Header component with login button", () => {
   render(<Header />, { wrapper: BrowserRouter });
 
-    const heading = screen.getByRole("button",{name: "Login"});
+    const heading = screen.getByRole("button",{name: "Login"}); // if there are multiple buttons and specifies name of the button
     expect(heading).toBeInTheDocument();
+});
+it("Should render Header component with Cart Text", () => {
+  render(<Header />, { wrapper: BrowserRouter });
+
+    const heading = screen.getByText(/Cart/);  //regex which just see the Cart text.
+    expect(heading).toBeInTheDocument()
 });
